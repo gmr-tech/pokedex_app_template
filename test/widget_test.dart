699e688016856af1a7480bb01277b8ac592d08d0
Igor Maldonado <igormaldonado77@gmail.com>
2023-07-17 14:27:48 -0300

    PAT-30: isolate main file to fix linter warnings

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pokedex_app_template/main.dart';
+import 'package:pokedex_app_template/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

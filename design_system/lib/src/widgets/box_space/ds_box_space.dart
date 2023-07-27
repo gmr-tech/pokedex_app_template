eafb9ae0f042337c336c762046735139d389f43b
Renato Marinho Gonçalves <gmr-renato@gmail.com>
2023-07-27 10:03:17 -0300

    PAT-19: update doc examples

--- design_system/lib/src/widgets/box_space/ds_box_space.dart
+++ design_system/lib/src/widgets/box_space/ds_box_space.dart
@@ -4,6 +4,12 @@ import '../../theme/constant/ds_const_space.dart';
 
 part 'ds_box_space_size.dart';
 
+/// A classe DSBoxSpace facilita a criação de SizedBox com dimensões
+/// pré-definidas, evitando a necessidade de escrever manualmente o
+/// SizedBox com os valores de altura e largura em cada caso. Ela fornece
+/// construtores para tamanhos específicos e também permite a criação de
+/// caixas com dimensões personalizadas usando a enumeração DSBoxSpaceSize.
+///
 /// {@template DSBoxSpace}
 /// A class to simpligy the usage of:
 /// ```dart
@@ -15,6 +21,7 @@ part 'ds_box_space_size.dart';
 /// ````
 /// Only values from [DSBoxSpaceSize] are available for dimension.
 /// {@endtemplate}
+///
 class DSBoxSpace extends SizedBox {
   ///{@macro DSBoxSpace}
   const DSBoxSpace({super.key}) : super.square(dimension: DSConstSpace.medium);
@@ -47,8 +54,3 @@ class DSBoxSpace extends SizedBox {
   DSBoxSpace.custom({required DSBoxSpaceSize size, super.key})
       : super.square(dimension: size.kSize);
 }
-/// a classe DSBoxSpace facilita a criação de SizedBox com dimensões 
-/// pré-definidas, evitando a necessidade de escrever manualmente o 
-/// SizedBox com os valores de altura e largura em cada caso. Ela fornece 
-/// construtores para tamanhos específicos e também permite a criação de 
-/// caixas com dimensões personalizadas usando a enumeração DSBoxSpaceSize.
--- lib/src/common/presentation/app_assets.dart
+++ lib/src/common/presentation/app_assets.dart
@@ -1,23 +1,23 @@
 // ignore_for_file: public_member_api_docs
 
-///Essa classe e a constante podem ser úteis para armazenar 
-///informações sobre ativos ou recursos utilizados no aplicativo.
-///Aqui estão algumas possíveis utilizações:
-
-///Referenciar pacotes personalizados: A constante designSystemPackage 
-///pode ser usada para referenciar o nome de um pacote personalizado chamado
-///"design_system". Isso pode ser útil ao importar classes, arquivos 
-///ou recursos desse pacote em outros lugares do código. Ao usar essa constante
-///em vez de uma string direta, você evita erros de digitação e torna a
-///referência mais legível e fácil de manter.
-
-///Acessar caminhos de arquivos ou URLs: Em casos em que você precise
-///especificar o caminho de um arquivo ou URL relacionado ao pacote 
-///"design_system", essa constante pode ser usada para construir o caminho 
-///completo ou URL. Por exemplo, se houver um arquivo de imagem chamado logo.png
-///no pacote "design_system", você pode usar AppAssets.designSystemPackage + 
-///'/logo.png' para obter o caminho completo.
-
+/// Classe que contém os caminhos dos assets da aplicação.
+/// Para acessar os assets, basta utilizar o caminho:
+///
+/// ```
+/// AppAssets.designSystemPackage + '/assets/images/logo.png'
+/// ```
+///
+/// Onde `AppAssets.designSystemPackage` é o caminho para o pacote
+/// `design_system` e `/assets/images/logo.png` é o caminho para o
+/// asset desejado.
+///
+/// Para acessar os assets de outros pacotes, basta criar uma nova
+/// constante com o nome do pacote e o caminho para o asset desejado.
+///
+/// ```
+/// static const String designSystemPackage = 'design_system';
+/// ```
+///
 class AppAssets {
   static const String designSystemPackage = 'design_system';
 }
